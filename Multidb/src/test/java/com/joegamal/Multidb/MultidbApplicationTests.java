ce67b10e3cdbcb6b5e2447b300d77425a351233f
joegamal <yusufmagamalth@gmail.com>
2026-05-18 04:09:39 +0300

    basic start skeleton

---
+++ Multidb/src/test/java/com/joegamal/Multidb/MultidbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joegamal.Multidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultidbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

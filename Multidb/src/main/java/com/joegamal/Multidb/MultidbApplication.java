55a06031da76a2fc7bd728dbcc7cdfcf240cc55f
joegamal <yusufmagamalth@gmail.com>
2026-05-24 17:22:46 +0300

    a change

--- Multidb/src/main/java/com/joegamal/Multidb/MultidbApplication.java
+++ Multidb/src/main/java/com/joegamal/Multidb/MultidbApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultidbApplication {
+public class MultidbApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 
 		SpringApplication.run(MultidbApplication.class, args);
 		//pop is pops
